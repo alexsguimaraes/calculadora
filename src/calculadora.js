529c9c0076b2414a26180f4d2a5fef29551ccc96
Alex Guimaraes <alex.guimaraes.SPASSU@petrobras.com.br>
2020-12-03 00:17:42 -0300

    Interface

---
+++ src/calculadora.js
@@ -0,0 +1,88 @@
+import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap';
+import './calculadora.css';
+
+function Calculadora() {
+  return (
+    <Jumbotron style ={{
+      background: 'transparent !important', 
+      backgroundColor: '#007bff', 
+      padding: '5px', 
+      margin: '5px',
+      with: '400px'
+    }}>
+      <Container>
+        <Row>
+          <Col xs="3">
+            <Button variant="danger">C</Button>
+          </Col>
+
+          <Col xs="9">
+            <Form.Control type="text"
+            name="txtNumeros"
+            class="text-right"
+            readOnly="readOnly" />  
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <Button variant="light">7</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">8</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">9</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="warning">/</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <Button variant="light">4</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">5</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">6</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="warning">*</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <Button variant="light">1</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">2</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">3</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="warning">-</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <Button variant="light">0</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="light">.</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="success">=</Button>
+          </Col>
+          <Col xs="3">
+            <Button variant="warning">+</Button>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+
+  );
+}
+
+export default Calculadora;
